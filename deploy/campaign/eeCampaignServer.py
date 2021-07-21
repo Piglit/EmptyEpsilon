b9354c852b4342d2c57d01c37dd1ca8c9125c954
Pithlit <piglit@mail.de>
2021-07-21 14:57:17 +0200

    campaign-server: implemented

---
+++ deploy/campaign/eeCampaignServer.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+from fastapi import FastAPI, Request, Body, logger
+from fastapi.testclient import TestClient
+from pydantic import BaseModel
+from typing import Optional, Literal, List
+from pprint import pprint
+import uvicorn
+import logging
+import os
+import json
+from copy import deepcopy
+
+from scenarioInfo import scenarioInfos
+from srvData import servers
+
+#MISSIONS = missionDB.MISSIONS
+
+# note: uvicorn logging is a bitch
+logging.getLogger("uvicorn.access").handlers = []
+logging.getLogger("uvicorn.access").propagate = False
+
+log = logging.getLogger(__name__)
+h = logging.StreamHandler()
+h.setFormatter(logging.Formatter("%(asctime)s\t%(levelname)s:\t%(message)s", datefmt="%H:%M:%S"))
+log.setLevel("DEBUG")
+log.addHandler(h)
+
+app = FastAPI()
+testClient = TestClient(app)
+
+def scenarioFileNameToMissionId(scenario_name):
+	return scenario_name.removeprefix("scenario_").removesuffix(".lua")
+
+@app.get("/")
+async def root():
+	return {"message": "Hello Space"}
+
+@app.post("/debug")
+async def postDebug(request : Request):
+	log.debug(request.method)
+	log.debug(request.url)
+	log.debug(request.headers)
+	log.debug(request.query_params)
+	log.debug(request.path_params)
+	j = await request.body()
+	log.debug(j)
+	j = await request.json()
+	log.debug(j)
+
+class EEServerScenarioInfo(BaseModel):
+	filename: str
+	name: str
+	variation: str
+
+	def getId(self):
+		return scenarioFileNameToMissionId(self.filename)
+
+	def __str__(self):
+		readable = self.name
+		if self.variation and self.variation != "None":
+			readable += " ["+self.variation+"]"
+		if logging.DEBUG >= log.level:
+			readable += " ("+self.getId() + "; " + self.filename+")"
+		return readable
+
+class ScenarioInfoVictory(EEServerScenarioInfo):
+	faction: str
+
+class ScenarioInfoScriptMessage(EEServerScenarioInfo):
+	script_message: str
+
+def runScenarioInfoCallback(scenario_id, callback_name, server_name):
+	info = scenarioInfos.get(scenario_id)
+	if info and callback_name in info:
+		if isinstance(info[callback_name], list):
+			for cb in info[callback_name]:
+				cb(server_name)
+		else:
+			info[callback_name](server_name)
+
+@app.post("/scenario_start")
+async def scenario_start(scenario_info: EEServerScenarioInfo, server_name: str = Body(...)):
+	log.info(server_name + "\tstarted scenario" + str(scenario_info))
+	runScenarioInfoCallback(scenario_info.getId(), "@start", server_name)
+
+@app.post("/scenario_end")
+async def scenario_end(scenario_info: EEServerScenarioInfo, server_name: str = Body(...)):
+	log.info(server_name + "\taborted scenario" + str(scenario_info))
+	runScenarioInfoCallback(scenario_info.getId(), "@end", server_name)
+
+@app.post("/scenario_victory")
+async def scenario_victory(scenario_info: ScenarioInfoVictory, server_name: str = Body(...)):
+	victory_faction = scenario_info.faction
+	log.info(server_name + "\tfinished scenario " + str(scenario_info) + "\twinner: " + victory_faction)
+	call = "@victory["+victory_faction+"]"
+	runScenarioInfoCallback(scenario_info.getId(), call, server_name)
+
+@app.post("/script_message")
+async def script_message(scenario_info: ScenarioInfoScriptMessage, server_name: str = Body(...)):
+	scm = scenario_info.script_message
+	log.info(server_name + "\tscript message   " + str(scenario_info) + "\t:" + scm)
+	if scm.startswith("unlock:[") and scm.endswith("]"):
+		unlock = scm.split("[", maxsplit=1)[1]
+		unlock = unlock.strip("]")
+		unlock = unlock.split(",")
+		unlock = list(map(strip,unlock))
+		unlockMissions(notification.server_name, unlock)
+
+class ScenarioResponse(BaseModel):
+	scenarios: List[str]
+
+@app.get("/scenarios/{server_name}", response_model = ScenarioResponse)
+async def getScenarios(server_name):
+	log.debug(server_name + "\tget scenarios")
+	scenarios = servers.getScenarios(server_name)
+	scenarios = ["scenario_"+s+".lua" for s in scenarios] 
+	return {"scenarios": scenarios}
+
+@app.get("/scenario_info/{server_name}/{scenario_name}")
+async def getScenarioInfo(server_name, scenario_name):
+	log.debug(server_name + "\tget scenario info for "+scenario_name)
+	scenario_name = scenarioFileNameToMissionId(scenario_name)
+	info = deepcopy(scenarioInfos[scenario_name]["info"])
+	variations = servers.getScenarioVariations(scenario_name, server_name)
+	#EE expects key = variation[name], value = descr
+	if "*" in variations:
+		for v, descr in scenarioInfos[scenario_name]["variations"].items():
+			info["variation["+v+"]"] = descr
+	else:
+		for v in variations:
+			if v is not None:
+				# we can currently not forbid the default variation in EE
+				descr = scenarioInfos[scenario_name]["variations"].get(v, "no description available")
+				info["variation["+v+"]"] = descr
+	info = {"scenarioInfo": info}
+	#log.debug(json.dumps(info))
+	return info
+
+if __name__ == "__main__":
+	uvicorn.run("eeCampaignServer:app", host="0.0.0.0", reload=True, port=8888)
---
+++ deploy/campaign/scenarioInfo.py
@@ -0,0 +1,118 @@
+"""
+What is it for?
+
+EE-Srv asks for scenarios
+CS answeres with list of scenarios
+EE-Srv asks for a scenario-info
+CS answeres with scenario infos
+EE-Srv starts scenario
+CS handles @start events
+EE-Srv sends script events
+CS handles @script events
+EE-Srv sends victory event
+CS handles @victory event
+EE-Srv sends end event
+CS handles @end event
+
+Clever Datastructures:
+
+scenarios are addressed by filename
+-> Dict: filename -> scenarioInfos, scenarioCampaignInfos
+-> scenarioInfos contain (read from script): Name, Type, Description. But not Variations
+-> unusedScenarioCampaignInfos contain: shipType (unused?), all Variations (read from script)
+-> scenarioCampaignInfos contain: @event callbacks: @start, @script(msg), @victory(faction), @end
+-> callbacks typically contain unlocks (script, variation, multiplayerShipType)
+
+srv-specific file with: scenario ID (filename) + list of unlocked variations
+
+"""
+from functools import partial as P
+from srvData import servers as S
+
+scenarioInfos = {
+	"00_test":				{"@start": P(S.unlockScenario, "01_test2"), "@end": P(S.unlockShip, "Hathcock"), "@victory[Human Navy]": P(S.unlockScenario, "09_outpost")},
+	"01_test2":				{},
+	"00_basic":				{},
+	"01_quick_basic":		{},
+	"20_training1":			{"@victory[Human Navy]": [P(S.unlockScenarios, [("21_training2", "*"), ("22_training3", "*"), ("23_training4", "*"), "01_quick_basic"]), P(S.unlockShip, "Phobos")]},
+	"21_training2":			{"@victory[Human Navy]": [P(S.unlockScenario, "09_outpost"), P(S.unlockShip, "Hathcock")]},
+	"22_training3":			{"@victory[Human Navy]": P(S.unlockShip, "Piranha M5P")},
+	"23_training4":			{"@victory[Human Navy]": P(S.unlockShip, "Nautilus")},
+	"01a_waves":			{},
+	"01d_waves":			{},
+	"01e_waves":			{},
+	"02_beacon":			{},
+	"03_edgeofspace":		{},
+	"04_gftp":				{},
+	"05_surrounded":		{},
+	"06_battlefield":		{},
+	"08_atlantis":			{},
+	"09_outpost":			{},
+	"50_gaps":				{},
+	"51_deliverAmbassador":	{},
+	"53_escape":			{},
+	"55_defenderHunter":	{},
+	"57_shoreline":			{},
+}
+
+for key in scenarioInfos:
+	filename = "scenario_"+key+".lua"
+	path = "../../scripts-piglit/"+filename
+	scenarioInfos[key]["info"] = {}
+	scenarioInfos[key]["variations"] = {}
+	assert "@victory" not in scenarioInfos[key], "keyword @victory is not allowed without a faction"
+	
+	with open(path,"r") as file:
+		state = None
+		for line in file:
+			if state == "brief":
+				if not line.startswith("---"):
+					state = None
+				else:
+					scenarioInfos[key]["info"]["Description"] += "\n"+line[3:].strip()
+					continue
+			if not line.startswith("--"):
+				break
+			line = line[2:].strip()
+			if line.startswith("Name:") and "name" not in scenarioInfos[key]:
+				scenarioInfos[key]["info"]["Name"] = line.split(":",maxsplit=1)[1].strip()
+			if line.startswith("Type:"):
+				scenarioInfos[key]["info"]["Type"] = line.split(":",maxsplit=1)[1].strip()
+			if line.startswith("Description:"):
+				scenarioInfos[key]["info"]["Description"] = line.split(":",maxsplit=1)[1].strip()
+				state = "brief"
+			if line.startswith("Variation["):
+				var, descr = line.split("[", maxsplit=1)[1].split("]", maxsplit=1)
+				descr = descr.removeprefix(":").strip()
+				scenarioInfos[key]["variations"][var] = descr
+
+					
+scenarioInfos_unused_infos = {
+"scenario_00_test.lua":					{"shipType": "Atlantis", "unlocks":["test2"]},
+"scenario_01_test2.lua":				{"shipType": "Atlantis", "unlocks":[]},
+"scenario_00_basic.lua":				{"shipType": "Atlantis", "unlocks":[]},
+"scenario_20_training1.lua":			{"shipType": "Phobos", "unlocks":["training2", "training3", "training4", "quick basic"]},
+"scenario_00_training2.lua":			{"shipType": "Hathcock", "unlocks":["outpost"]},
+"scenario_00_training3.lua":			{"shipType": "Piranha", "unlocks":["training3 boss", "surrounded"]},
+"scenario_00_training3.lua":			{"variation": "Boss", "name": "Training: Missile Cruiser - Boss", "shipType": "Piranha", "unlocks":[]},
+"scenario_00_training4.lua":			{"shipType": "Nautilus", "unlocks":["gaps"]},
+"scenario_01a_waves.lua":				{"shipType": "", "unlocks":[]},
+"scenario_01d_waves.lua":				{"shipType": "", "unlocks":[]},
+"scenario_01e_waves.lua":				{"shipType": "", "unlocks":[]},
+"scenario_02_beacon.lua":				{"shipType": "Atlantis", "unlocks":[]},
+"scenario_03_edgeofspace.lua":			{"shipType": "Phobos", "unlocks":[]},
+"scenario_04_gftp.lua":					{"shipType": "Phobos", "unlocks":[]},
+"scenario_05_surrounded.lua":			{"shipType": "Piranha", "unlocks":[]},
+"scenario_06_battlefield.lua":			{"shipType": "", "unlocks":[]},
+"scenario_07_quick_basic.lua":			{"shipType": "Phobos", "unlocks":["quick basic advanced"]},
+"scenario_07_quick_basic.lua":			{"variation": "Advanced", "name": "Quick Basic - Advanced", "shipType": "Atlantis", "unlocks":[]},
+"scenario_08_atlantis.lua":				{"shipType": "Atlantis", "unlocks":[]},
+"scenario_09_outpost.lua":				{"shipType": "Hathcock", "unlocks":[]},
+"scenario_50_gaps.lua":					{"shipType": "Nautilus", "unlocks":[]},
+"scenario_51_deliverAmbassador.lua":	{"shipType": "Flavia", "unlocks":[]},
+"scenario_53_escape.lua":				{"shipType": "", "unlocks":[]},
+"scenario_55_defenderHunter.lua":		{"shipType": "", "unlocks":[]},
+"scenario_57_shoreline.lua":			{"shipType": "", "unlocks":[]},
+}
+
+

6ebba29672199bc1f56335bd4b31f53e1dcf1307
Pithlit <Pithlit@mail.de>
2022-10-04 21:05:19 +0200

    campaignRoundTimer

--- deploy/campaign/watchStatus.py
+++ deploy/campaign/watchStatus.py
@@ -2,14 +2,22 @@
 import Pyro4
 from time import sleep
 from datetime import datetime
+
 campaign = Pyro4.Proxy("PYRONAME:campaign_state")
 format = "{:<16}{:<24}{}"
+timer = Pyro4.Proxy("PYRONAME:round_timer")
+
 while(True):
     # clear term
     sleep(1)
     print(chr(27)+"[2J")
     print("Time: "+datetime.now().strftime("%H:%M:%S"))
     stati = campaign.getStatusAll()
+    try:
+        t = timer.getTimer()
+        print(f"{t['state']} {t['left']} ({t['until']})")
+    except:
+        pass
     print(format.format("Ship", "Mission", "Progress"))
     print(80*"-")
     for srv, status in stati.items():

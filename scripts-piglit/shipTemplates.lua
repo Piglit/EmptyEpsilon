1788e61c27727c5ca03fb20cb15d01d8f1b2f1bc
Pithlit <piglit@mail.de>
2022-11-02 19:56:27 +0100

    scripts: rebase scripts, except scenarios, from upstream
    * comms
    * models
    * science_db
    * shipTemplates
    * utils

--- scripts-piglit/shipTemplates.lua
+++ scripts-piglit/shipTemplates.lua
@@ -2,14 +2,25 @@
 Base of the ship templates, from here different classes of ships are included to be used within the game.
 Each sub-file defines it's own set of ship classes.
 
-Difference to daid's version: the ships are sorted by faction
---]]
+These are:
+* Stations: For different kinds of space stations, from tiny to huge.
+* Starfighters: Smallest ships in the game.
+* Frigates: Medium sized ships. Operate on a small crew.
+* Corvette: Large, slower, less maneuverable ships.
+* Dreadnaught: Huge things. Everything in here is really really big, and generally really really deadly.
+* Exuari: Ships with a similar style, designed (but not limited) for the Exuari faction.
 
-require("shipTemplates_Stations.lua")
-require("shipTemplates_Transports.lua")
-require("shipTemplates_Player.lua")
-require("shipTemplates_Humans.lua")
-require("shipTemplates_Exuari.lua")
-require("shipTemplates_Kraylor.lua")
-require("shipTemplates_Ktlitan.lua")
-require("shipTemplates_special.lua")
+Player ships are in general large frigates to small corvette class
+--]]
+require("shiptemplates/stations.lua")
+require("shiptemplates/transport.lua")
+require("shiptemplates/player.lua")
+require("shiptemplates/humans.lua")
+require("shiptemplates/exuari.lua")
+require("shiptemplates/kraylor.lua")
+require("shiptemplates/ktlitan.lua")
+require("shiptemplates/satellites.lua")
+require("shiptemplates/special.lua")
+
+--For now, we add our old ship templates as well. These should be removed at some point.
+--require("shiptemplates/OLD.lua")

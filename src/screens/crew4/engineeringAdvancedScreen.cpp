9f0b452b2f036097cf8f8c227dc443a5643dc6a5
Pithlit <piglit@mail.de>
2023-07-27 16:14:14 +0200

    tactical/engi+: move shield and beam config around
    tactical can also activate shields now (playable without engineer+)
    beam frequency move to engineering+

--- src/screens/crew4/engineeringAdvancedScreen.cpp
+++ src/screens/crew4/engineeringAdvancedScreen.cpp
@@ -3,6 +3,9 @@
 #include "gameGlobalInfo.h"
 #include "screenComponents/shieldFreqencySelect.h"
 #include "screenComponents/shieldsEnableButton.h"
+#include "screenComponents/powerDamageIndicator.h"
+#include "screenComponents/beamFrequencySelector.h"
+#include "gui/gui2_label.h"
 
 EngineeringAdvancedScreen::EngineeringAdvancedScreen(GuiContainer* owner)
 : EngineeringScreen(owner, engineeringAdvanced)
@@ -11,7 +14,15 @@ EngineeringAdvancedScreen::EngineeringAdvancedScreen(GuiContainer* owner)
     {
         //The shield frequency selection includes a shield enable button.
         (new GuiShieldFrequencySelect(this, "SHIELD_FREQ"))->setPosition(20, 310, sp::Alignment::TopLeft)->setSize(240, 100);
+
+        GuiElement* beam_info_box = new GuiElement(this, "BEAM_INFO_BOX");
+        beam_info_box->setPosition(20, 410, sp::Alignment::TopLeft)->setSize(240, 50);
+        (new GuiLabel(beam_info_box, "BEAM_INFO_LABEL", tr("Beams"), 30))->addBackground()->setPosition(20, 0, sp::Alignment::TopLeft)->setSize(80, 50);
+        (new GuiBeamFrequencySelector(beam_info_box, "BEAM_FREQUENCY_SELECTOR"))->setPosition(120, 0, sp::Alignment::TopLeft)->setSize(120, 50);
+        (new GuiPowerDamageIndicator(beam_info_box, "", SYS_BeamWeapons, sp::Alignment::CenterLeft))->setPosition(0, 0, sp::Alignment::BottomLeft)->setSize(240, 50);
     }else{
         (new GuiShieldsEnableButton(this, "SHIELDS_ENABLE"))->setPosition(20, 310, sp::Alignment::TopLeft)->setSize(240, 50);
     }
+
+
 }

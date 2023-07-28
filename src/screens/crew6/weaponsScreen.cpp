9fe70dea028091de099fc7c2e417857061ec3597
Pithlit <piglit@mail.de>
2023-07-28 18:12:17 +0200

    hide gui elements when system is not available
    beams, missiles, shields, aimLock

--- src/screens/crew6/weaponsScreen.cpp
+++ src/screens/crew6/weaponsScreen.cpp
@@ -57,7 +57,7 @@ WeaponsScreen::WeaponsScreen(GuiContainer* owner)
 
     lock_aim = new AimLockButton(this, "LOCK_AIM", tube_controls, missile_aim);
     lock_aim->setPosition(250, 20, sp::Alignment::TopCenter)->setSize(130, 50);
-    if (my_spaceship->hasSystem(SYS_BeamWeapons) && (gameGlobalInfo->use_beam_shield_frequencies || gameGlobalInfo->use_system_damage))
+    if (my_spaceship && my_spaceship->hasSystem(SYS_BeamWeapons) && (gameGlobalInfo->use_beam_shield_frequencies || gameGlobalInfo->use_system_damage))
     {
         GuiElement* beam_info_box = new GuiElement(this, "BEAM_INFO_BOX");
         beam_info_box->setPosition(-20, -120, sp::Alignment::BottomRight)->setSize(280, 150);

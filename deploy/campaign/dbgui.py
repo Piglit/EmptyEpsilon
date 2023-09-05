058d65648e8f356067cbbddbf8fb1041e0517c83
Pithlit <Pithlit@mail.de>
2023-09-05 19:20:48 +0200

    command gui: add round timer manipulation

--- deploy/campaign/dbgui.py
+++ deploy/campaign/dbgui.py
@@ -1,15 +1,39 @@
 #!/usr/bin/python3
-import PySimpleGUI as g
+"""
+    Tabs: ships(servers), scenarios(campaign)
+    ships:
+        Status/Mission+Progress
+        Unlocked Scenarios + Settings (add/del)
+        Unlocked Ships (add/del)
+        Additional Code (modify)
+        Some score or so...
+    scenarios
+        (filename)
+        Name
+        Events -> unlocks + Settings
+    every change saves to file
+    every campaign change triggers refresh
+"""
+import PySimpleGUI as sg
 import json
+import pyrohelper
+import Pyro4
+from datetime import datetime
 
 SCENARIO_DIR = "../../scripts-piglit/"
 
-g.set_options(font=("Arial Bold",14))
+sg.set_options(font=("Arial Bold",14))
 
+# Timer
+timerSource = Pyro4.Proxy("PYRONAME:round_timer")
+timerNow = [sg.T("Time:", size=6), sg.T("00:00:00", key="timer.now")]
+timerRound = [sg.T("", key="timer.state", size=6), sg.T("", key="timer.left"), sg.T("", key="timer.until"), sg.B("Skip", key="timer.skip", visible=False), sg.B("Edit", key="timer.edit", visible=False)]
+
+# Ship Tree
 with open("serverDB.json", "r") as file:
     data = json.load(file)
 
-treedata = g.TreeData()
+treedata = sg.TreeData()
 
 def translate_scenario_name(scenario):
     try:
@@ -46,10 +70,10 @@ for ship, shipData in data.items():
         treedata.Insert(ship+"_ships", shipAvail, shipAvail, [])
 
 
-tree=g.Tree(data=treedata,
+tree=sg.Tree(data=treedata,
         headings=['status', 'scenario', 'progress'],
         auto_size_columns=True,
-        select_mode=g.TABLE_SELECT_MODE_EXTENDED,
+        select_mode=sg.TABLE_SELECT_MODE_EXTENDED,
         num_rows=10,
         col0_width=15,
         key='-TREE-',
@@ -58,10 +82,76 @@ tree=g.Tree(data=treedata,
         expand_x=True,
         expand_y=True,
         )
-layout=[[tree]]
-window=g.Window("Tree Demo", layout, size=(1200, 800), resizable=True)
+
+
+
+def error(msg, window=None):
+    if window:
+        window.hide()
+    sg.Window("Error", [[sg.T("Error: "+msg)], [sg.B("OK")]]).read(close=True)
+    if window:
+        window.un_hide()
+
+def confirm(msg, window=None):
+    if window:
+        window.hide()
+    e,v = sg.Window("Confirm", [[sg.T(msg)], [sg.B("OK", key="ok"), sg.B("Cancel", key="cancel")]]).read(close=True)
+    if window:
+        window.un_hide()
+    return e == "ok"
+
+
+layout=[timerNow, timerRound, [tree]]
+window=sg.Window("Flottenkommando", layout, size=(1200, 800), resizable=True, text_justification="r")
 while True:
-    event, values = window.read()
-    print ("event:",event, "values:",values)
-    if event == g.WIN_CLOSED:
+    event, values = window.read(timeout=1000)
+    if event == sg.WIN_CLOSED:
        break
+    elif event == "timer.skip":
+        if confirm("Skip current phase?", window):
+            timerSource.nextPhase()
+    elif event == "timer.edit":
+        window.hide()
+        layoutTimerEdit = [
+                [sg.Push(), sg.T("Round time:"), sg.Input(timerSource.getRoundTime()//60, size=4, key="timer.round_time"), sg.T("minutes")],
+                [sg.Push(), sg.T("Pause time:"), sg.Input(timerSource.getPauseTime()//60, size=4, key="timer.pause_time"), sg.T("minutes")],
+                [sg.B("OK", k="ok"), sg.B("Cancel", k="cancel")]
+        ]
+        windowTimerEdit = sg.Window("Edit timer", layoutTimerEdit, text_justification="r")
+        while True:
+            event2, values2 = windowTimerEdit.read()
+            if event2 == "ok":
+                try:
+                    round_time = int(values2["timer.round_time"])
+                    pause_time = int(values2["timer.pause_time"])
+                    timerSource.setRoundTime(60*round_time)
+                    timerSource.setPauseTime(60*pause_time)
+                except:
+                    event2 = "error"
+                    error("Input must be a number", windowTimerEdit)
+            if event2 in (sg.WIN_CLOSED, "cancel", "ok"):
+                windowTimerEdit.close()
+                window.un_hide()
+                break
+    elif event == "__TIMEOUT__":
+        pass
+    else:
+        print ("event:",event, "values:",values)
+    # update timer
+
+    try:
+        t = timerSource.getTimer()
+        window["timer.now"].update(t["now"])
+        window["timer.state"].update(t["state"]+":")
+        window["timer.left"].update(t["left"])
+        window["timer.until"].update(f"({t['until']})")
+        window["timer.state"].unhide_row()
+        window["timer.skip"].update(visible=True)
+        window["timer.edit"].update(visible=True)
+    except:
+        window["timer.now"].update(datetime.now().strftime("%H:%M:%S"))
+        window["timer.state"].hide_row()
+        window["timer.state"].update("")
+        window["timer.left"].update("")
+        window["timer.until"].update("")
+

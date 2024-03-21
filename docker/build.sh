71bc511cefe75922f2b769209155ffc39be5d7a9
Pithlit <piglit@mail.de>
2024-03-21 13:50:18 +0200

    Set Version and cicd branch to shattered horizon

--- docker/build.sh
+++ docker/build.sh
@@ -11,7 +11,7 @@ echo "GITHUB_REF: ${GITHUB_REF}"
 echo "GITHUB_HEAD_REF: ${GITHUB_HEAD_REF}"
 echo "GITHUB_BASE_REF: ${GITHUB_BASE_REF}"
 
-GIT_REF_NAME_LIST=( "${GITHUB_HEAD_REF}" "${GITHUB_BASE_REF}" "${GITHUB_REF}" "master" )
+GIT_REF_NAME_LIST=( "${GITHUB_HEAD_REF}" "${GITHUB_BASE_REF}" "${GITHUB_REF}" "shattered_horizon24" )
 for git_ref_name in "${GIT_REF_NAME_LIST[@]}"
 do
   if [ -z "${git_ref_name}" ]; then
@@ -23,8 +23,8 @@ do
     echo "Skip [${git_ref_name}]"
     continue
   fi
-  SERIOUS_PROTON_BRANCH="${git_ref_name}"
-  output="$(git ls-remote --heads https://github.com/Daid/SeriousProton "${SERIOUS_PROTON_BRANCH}")"
+  SERIOUS_PROTON_BRANCH="shattered_horizon"
+  output="$(git ls-remote --heads https://github.com/Piglit/SeriousProton "${SERIOUS_PROTON_BRANCH}")"
   if [ -n "${output}" ]; then
     echo "Found SeriousProton branch [${SERIOUS_PROTON_BRANCH}]."
     break
@@ -35,7 +35,7 @@ done
 
 echo "Using SeriousProton branch ${SERIOUS_PROTON_BRANCH} ..."
 
-git clone --depth=1 -b "${SERIOUS_PROTON_BRANCH}" https://github.com/Daid/SeriousProton.git "${PROJECT_DIR}"/SeriousProton
+git clone --depth=1 -b "${SERIOUS_PROTON_BRANCH}" https://github.com/Piglit/SeriousProton.git "${PROJECT_DIR}"/SeriousProton
 
 mkdir build
 cd build

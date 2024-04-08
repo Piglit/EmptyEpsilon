52537f0c8ff097895ae76758c53a5c25cb0a6e68
Pithlit <Pithlit@mail.de>
2024-04-08 14:44:15 +0200

    mumble positional audio - first tries

---
+++ src/mumble.h
@@ -0,0 +1,49 @@
+#ifndef MUMBLE_H 
+#define MUMBLE_H
+
+#include "Updatable.h"
+
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <sys/mman.h>
+    #include <fcntl.h> /* For O_* constants */
+#endif // _WIN32
+
+struct LinkedMem {
+#ifdef _WIN32
+    UINT32   uiVersion;
+    DWORD    uiTick;
+#else
+    uint32_t uiVersion;
+    uint32_t uiTick;
+#endif
+    float    fAvatarPosition[3];
+    float    fAvatarFront[3];
+    float    fAvatarTop[3];
+    wchar_t  name[256];
+    float    fCameraPosition[3];
+    float    fCameraFront[3];
+    float    fCameraTop[3];
+    wchar_t  identity[256];
+#ifdef _WIN32
+    UINT32    context_len;
+#else
+    uint32_t context_len;
+#endif
+    unsigned char context[256];
+    wchar_t description[2048];
+};
+
+LinkedMem *lm = NULL;
+
+class MumblePositionalAudio : public Updatable
+{
+    explicit MumblePositionalAudio();
+    ~MumblePositionalAudio();
+    void update(float) override;
+
+private:
+    float updateDelay = 0.0f;
+}
+#endif//MUMBLE_H

ec0eb33f0a9d9314808a3e35bafa24f40397cb68
Pithlit <piglit@mail.de>
2024-04-14 13:20:52 +0200

    helms: finer control with keyboard or joystick
    A single button-press now changes the course by one degree (was 5 degree before).
    Analoge controls (joystick or controller axis) allow an even finer tuning of the turn rate.

--- src/screens/crew4/tacticalScreen.cpp
+++ src/screens/crew4/tacticalScreen.cpp
@@ -143,7 +143,7 @@ void TacticalScreen::onUpdate()
 {
     if (my_spaceship && isVisible())
     {
-        auto angle = (keys.helms_turn_right.getValue() - keys.helms_turn_left.getValue()) * 5.0f;
+        auto angle = (keys.helms_turn_right.getValue() - keys.helms_turn_left.getValue());
         if (angle != 0.0f)
         {
             my_spaceship->commandTargetRotation(my_spaceship->getRotation() + angle);

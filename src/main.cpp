04236deaabbb43f6dea6d9e6d767b93e4e2d8a63
Pithlit <Pithlit@mail.de>
2025-02-01 12:41:28 +0100

    play tutorial before autoconnect to server
    If tutorial is gives as argument (with a tutorial file as value, like tutorial=00_all.lua), the game will start with this tutorial.
    When the tutorial is finished, the autoconnect handling will take over, connecting the client to a server and a specific ship and station.
    If no autoconnect options are given, the client will return to the main menu.
    
    This way it is possible to start client with a specific tutorial to play and then connect the client to the server.

--- src/main.cpp
+++ src/main.cpp
@@ -191,6 +191,7 @@ int main(int argc, char** argv)
 
     new DirectoryResourceProvider("resources/");
     new DirectoryResourceProvider("scripts/");
+    new DirectoryResourceProvider("scripts/tutorial/");
     PackResourceProvider::addPackResourcesForDirectory("packs/");
     if (getenv("HOME"))
     {
@@ -405,7 +406,13 @@ int main(int argc, char** argv)
     new SteamRichPresence();
 #endif //STEAMSDK
 
-    if (PreferencesManager::get("server_scenario") == "")
+    string tutorial = PreferencesManager::get("tutorial");   // use "00_all.lua" for all tutorials
+    if (tutorial != "")
+    {
+        LOG(DEBUG) << "Starting tutorial: " << tutorial;
+        new TutorialGame(false, tutorial);
+    }
+    else if (PreferencesManager::get("server_scenario") == "")
         returnToMainMenu(defaultRenderLayer);
     else
     {
@@ -486,10 +493,6 @@ void returnToMainMenu(RenderLayer* render_layer)
         if (crew_position < 0) crew_position = 0;
         if (crew_position > max_crew_positions) crew_position = max_crew_positions;
         new AutoConnectScreen(ECrewPosition(crew_position), PreferencesManager::get("autocontrolmainscreen").toInt(), PreferencesManager::get("autoconnectship", "solo"));
-    }
-    else if (PreferencesManager::get("tutorial").toInt())
-    {
-        new TutorialGame(true);
     }else{
         new MainMenu();
     }

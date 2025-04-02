b6ea23462e9463077b6e44b8cb4d8808ef004429
Pithlit <Pithlit@mail.de>
2025-04-02 18:36:55 +0200

    fix: laser bullets - energy damage type

--- src/spaceObjects/missiles/laser.cpp
+++ src/spaceObjects/missiles/laser.cpp
@@ -22,7 +22,7 @@ LaserMissileRed::LaserMissileRed(): MissileWeapon("LaserMissileRed", MissileWeap
 
 void LaserMissileGreen::hitObject(P<SpaceObject> object)
 {
-    DamageInfo info(owner, DT_Kinetic, getPosition());
+    DamageInfo info(owner, DT_Energy, getPosition());
     object->takeDamage(category_modifier * 2, info);
     P<ExplosionEffect> e = new ExplosionEffect();
     e->setSize(category_modifier * 10);
@@ -30,7 +30,7 @@ void LaserMissileGreen::hitObject(P<SpaceObject> object)
 }
 void LaserMissileRed::hitObject(P<SpaceObject> object)
 {
-    DamageInfo info(owner, DT_Kinetic, getPosition());
+    DamageInfo info(owner, DT_Energy, getPosition());
     object->takeDamage(category_modifier * 2, info);
     P<ExplosionEffect> e = new ExplosionEffect();
     e->setSize(category_modifier * 10);

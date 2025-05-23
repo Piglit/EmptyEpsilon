96f2e0e47573e6000099bf9cc238256124124be7
Pithlit <piglit@mail.de>
2025-05-23 19:56:31 +0200

    tutorial as pre-flight-check
    tutorial: changes for more modular ships
    tutorial locale: use preflight check terms
    add tutorial: tactical
    menus: add pre-flight-check menu

---
+++ src/menus/pfc.h
@@ -0,0 +1,16 @@
+#ifndef PFC_MENU_H
+#define PFC_MENU_H
+
+#include "gui/gui2_canvas.h"
+
+class GuiElement;
+class GuiLabel;
+class GuiButton;
+
+class PFCMenu : public GuiCanvas
+{
+public:
+    PFCMenu();
+};
+
+#endif//PFC_MENU_H

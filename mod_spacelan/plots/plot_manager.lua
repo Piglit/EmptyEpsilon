e88df63593e49700001834f6b423f46c98f87116
Pithlit <piglit@mail.de>
2025-07-17 18:21:24 +0200

    scenario: use plot_manager

---
+++ mod_spacelan/plots/plot_manager.lua
@@ -0,0 +1,92 @@
+--[[ The plot_manager calls init, update and callback functions of all managed plot_modules, whenever the function is called from the game.
+-- This is done to make scripting more modular. You can now have multiple script files, with their own init, update and callback functions, and they are called from here.
+--]]
+
+
+plot_manager = {
+	plot_modules_by_name = {}	-- unordered, used for script access
+	plot_modules_by_index= {}	-- this one is ordered
+}
+
+function plot_manager:init(plot_module_names)
+	-- require file and store global object in modules
+	for _, name in ipairs(plot_module_names) do
+		require("plots/"..name..".lua")
+		local module_table = _G[name]
+		assert(module_table ~= nil, name .. " is not a global table in plots/"..name..".lua")
+		self.plot_modules_by_name[name] = module_table
+		table.insert(self.plot_modules_by_index, module_table)
+	end
+
+	-- distribute callbacks to modules that have an onNewPlayerShip function
+	onNewPlayerShip(function(ship)
+		for _,module in ipairs(self.plot_modules_by_index) do
+			if module.onNewPlayerShip ~= nil then
+				module:onNewPlayerShip(ship)
+			end
+		end
+		ship:onProbeLaunch(plot_manager.onProbeLaunch)
+	end)
+
+	-- call init and initTest on all modules that have those functions
+	for _,module in ipairs(self.plot_modules_by_index) do
+		if module.init ~= nil then
+			module:init()
+		end
+		if TEST and module.initTest ~= nil then
+			module:initTest()
+		end
+	end
+
+	-- expose everything to storage for outside script access.
+	local storage = getScriptStorage()
+	storage["plot_manager"] = self
+end
+
+function plot_manager.onProbeLaunch(ship, probe)
+	for _,module in ipairs(plot_manager.plot_modules_by_index) do
+		if module.onProbeLaunch ~= nil then
+			module:onProbeLaunch(ship, probe)
+		end
+	end
+end
+
+function plot_manager.gm_main_menu()
+	clearGMFunctions()
+	for _,module in ipairs(plot_manager.plot_modules_by_index) do
+		if module.gm_menu ~= nil then
+			module:gm_menu()	-- create gm functions
+		end
+	end
+end
+
+-- global function for back to menu button
+function gm_menu_back()
+    addGMFunction(_("buttonGM", "Back to Main Menu"),plot_manager.gm_main_menu)
+end
+
+function plot_manager:update(delta)
+	-- only call getActivePlayerShips once per update
+	local active_player_ships = getActivePlayerShips()
+
+	-- call update function of all modules
+	for _,module in ipairs(self.plot_modules_by_index) do
+		if module.update ~= nil then
+			module:update(delta)
+		end
+		if TEST and module.updateTest ~= nil then
+			module:updateTest()
+		end
+		if module.updatePlayerShip ~= nil then
+			for _,ship in ipairs(active_player_ships) do
+				module:updatePlayerShip(delta, ship)
+			end
+		end
+	end
+end
+
+-- This is the global update function!
+-- make sure to not have another one defined elsewhere!
+--function update(delta)
+--	plot_manager:update(delta)
+--end
--- mod_spacelan/scenario_99_avp.lua
+++ mod_spacelan/scenario_99_avp.lua
@@ -2,11 +2,7 @@
 -- Type: Development
 -- Proxy: 192.168.2.3
 
-require "plots/wh_artifacts.lua"
-require "plots/wh_fleetcommand.lua"
-require "plots/wh_players.lua"
-
-require "plots/wh_stations.lua"
+require "plots/plot_manager.lua"
 
 --require "sandbox_error.lua"
 require "xansta_mods.lua"
@@ -22,38 +18,17 @@ TEST = true
 function init()
 	difficulty = 1	-- global var from xanstas stuff
 	init_constants_xansta()
-	plots = {
-		wh_fleetcommand,
-		wh_players,
-		wh_artifacts,
-		wh_stations,	--TODO adjust
-	}
-	onNewPlayerShip(function(ship)
-		for i,plot in ipairs(plots) do
-			if plot.onNewPlayerShip ~= nil then
-				plot:onNewPlayerShip(ship)
-			end
-		end
-	end)
 
-	for i,plot in ipairs(plots) do
-		assert(plot.init ~= nil, "plot "..i.." must have init()")
-		plot:init()
-		if TEST and plot.initTest ~= nil then
-			plot:initTest()
-		end
-	end
+	plot_manager:init({
+		"wh_fleetcommand",
+		"wh_players",
+		"wh_artifacts",
+		"wh_stations",	--TODO adjust
+	})
 end
 
 function update(delta)
-	for i,plot in ipairs(plots) do
-		if plot.update ~= nil then
-			plot:update(delta)
-		end
-		if TEST and plot.updateTest ~= nil then
-			plot:updateTest()
-		end
-	end
+	plot_manager:update(delta)
 	script_hangar.update(delta)
 end
 

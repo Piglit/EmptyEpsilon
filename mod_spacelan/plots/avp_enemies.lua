a653ec173f8efc243350535e485ea3b9d8a802f6
Pithlit <piglit@mail.de>
2025-08-01 17:49:20 +0200

    enemies: wip

--- mod_spacelan/plots/avp_enemies.lua
+++ mod_spacelan/plots/avp_enemies.lua
@@ -57,23 +57,41 @@ function EnemyModule:spawnShipAtPosition(template, position)
 	return ship, used_strength
 end
 
+function EnemyModule:getClassTemplate(class)
+	-- returns a template of a specific ship class.
+	-- ship classes must be defined when creating a new subclass of EnemyModule
+	assert (self.classes ~= nil)
+	assert (self.classes[class] ~= nil)
+	local class_templates = self.classes[class]
+	local class_index = self.classes[class.."_index"] or 0
+	class_index = class_index +1
+	if class_index > #class_templates then
+		class_templates = arrayShuffle(self.classes[class])
+		class_index = 1
+	end
+	self.classes[class.."_index"] = class_index
+	return class_templates[class_index]
+end
+
+--[[ Kraylor --]]
+
 EnemyModuleKraylor = EnemyModule:new{
 	faction="Kraylor",
-	breakers = arrayShuffle({
-		"Rockbreaker",
-		"Rockbreaker Merchant",
-		"Rockbreaker Murderer",
-		"Rockbreaker Mercenary",
-		"Rockbreaker Marauder",
-		"Rockbreaker Military",
-	}),
-	breaker_index = 0,
-	bringers = arrayShuffle({
-		"Deathbringer",
-		"Painbringer",
-		"Doombringer"
-	}),
-	bringer_index = 0,
+	classes={
+		breakers = arrayShuffle({
+			"Rockbreaker",
+			"Rockbreaker Merchant",
+			"Rockbreaker Murderer",
+			"Rockbreaker Mercenary",
+			"Rockbreaker Marauder",
+			"Rockbreaker Military",
+		}),
+		bringers = arrayShuffle({
+			"Deathbringer",
+			"Painbringer",
+			"Doombringer"
+		}),
+	}
 }
 
 function EnemyModuleKraylor:spawnEnemiesAtPositions(positions, strength)
@@ -92,7 +110,12 @@ function EnemyModuleKraylor:spawnEnemiesAtPositions(positions, strength)
 	end
 	flotilla_strength = flotilla_strength + base_strength
 	while flotilla_strength >= 50 do
-		last_strength = self:spawnFlotilla(positions[position_index], flotilla_strength)
+		if irandom(1,3) == 1 then
+			-- 33%: 4 Rockbreakers, 66%: 1 *bringer, 2 Rockbreakers
+			last_strength = self:spawnBreakerFlotilla(positions[position_index], flotilla_strength)
+		else
+			last_strength = self:spawnBringerFlotilla(positions[position_index], flotilla_strength)
+		end
 		flotilla_strength = flotilla_strength - last_strength
 		position_index = position_index % #positions + 1
 	end
@@ -104,62 +127,135 @@ function EnemyModuleKraylor:spawnEnemiesAtPositions(positions, strength)
 	end
 end
 
+function EnemyModuleKraylor:addDrones(ship, min, max, strength)
+	max = math.min(max, math.floor(strength / 5))
+	local drones_amount	= max
+	if min < max then
+		drones_amount = irandom(min,max)
+	end
+	if drones_amount > 0 then
+		script_hangar.create(ship, "Drone", drones_amount)
+		return 5 * drones_amount
+	end
+	return 0
+end
+
 function EnemyModuleKraylor:spawnMarauder(pos, strength)
+	-- spawn one Marauder. 50% Spinebreaker, 50% Rockbreaker
+	-- if enough strength is given, it may launch a drone on contact (50% chance)
 	local template = "Spinebreaker"
 	if strength < 24 then
 		template = "Rockbreaker"
 	elseif irandom(1,2) == 1 then
 		-- 50% Spinebreaker, 50% Rockbreaker
-		template = self:getBreakerTemplate()
+		template = self:getClassTemplate("breakers")
 	end
 	local ship, used_strength = self:spawnShipAtPosition(template, pos)
-	if strength - used_strength > 5 and irandom(1,2) == 1 then
-		-- 50% add drone in hangar
-		script_hangar.create(ship, "Drone", 1)
-		used_strength = used_strength + 5
-	end
+	ship:orderRoaming()
+	-- 50% add drone in hangar
+	used_strength = used_strength + self:addDrones(ship, 0, 1, strength - used_strength)
 	return used_strength
 end
 
-function EnemyModuleKraylor:spawnFlotilla(pos, strength)
-
+function EnemyModuleKraylor:spawnBreakerFlotilla(pos, strength)
+	-- spawns a small group of ships, flying in formation
+	local formationLeader, formationSecond
+	total_used_strength = 0
+	for index = 1, 4 do
+		local template = self:getClassTemplate("breakers")
+		local ship, used_strength = self:spawnShipAtPosition(template, pos)
+		if index == 1 then
+			ship:orderDefendLocation(pos[1], pos[2])
+		end
+		formationLeader, formationSecond = script_formation.buildFormationIncremental(ship, index, formationLeader, formationSecond)
+		total_used_strength = total_used_strength + used_strength
+		if total_used_strength >= strength then
+			break
+		end
+	end
+	return total_used_strength
 end
+
+function EnemyModuleKraylor:spawnBringerFlotilla(pos, strength)
+	-- spawns a small group of ships, flying in formation
+	-- lead by a stronger leader
+	local formationLeader, formationSecond
+	local template = self:getClassTemplate("bringers")
+	local leader, total_used_strength = self:spawnShipAtPosition(template, pos)
+	leader:orderDefendLocation(pos[1], pos[2])
+	formationLeader = leader
+	for index = 2, 3 do
+		if total_used_strength >= strength then
+			break
+		end
+		template = self:getClassTemplate("breakers")
+		local ship, used_strength = self:spawnShipAtPosition(template, pos)
+		formationLeader, formationSecond = script_formation.buildFormationIncremental(ship, index, formationLeader, formationSecond)
+		total_used_strength = total_used_strength + used_strength
+	end
+
+	total_used_strength = total_used_strength + self:addDrones(leader, 1, 2, strength - total_used_strength)
+	return total_used_strength
+end
+
 
 function EnemyModuleKraylor:spawnBase(pos, strength)
-
-end
-
-function EnemyModuleKraylor:getBreakerTemplate()
-	self.breaker_index = self.breaker_index +1
-	if self.breaker_index > #self.breakers then
-		self.breakers = arrayShuffle(self.breakers)
-		self.breaker_index = 1
-	end
-	return self.breakers[breaker_index]
-end
-
-function EnemyModuleKraylor:getBringerTemplate()
-	self.bringer_index = self.bringer_index +1
-	if self.bringer_index > #self.bringers then
-		self.bringers = arrayShuffle(self.bringers)
-		self.bringer_index = 1
-	end
-	return self.bringers[bringer_index]
-end
-
-
-function EnemyModuleKraylor:createShips(templates)
-	local formationLeader = nil
-	local formationSecond = nil
-	if #templates > 1 then
-		for index,template in ipairs(templates) do
-			local ship = CpuShip():setTemplate(template)
-			ship:setFaction(self.faction):setScannedByFaction(self.faction, true)
-			ship:orderRoaming()	-- only the first one, the other ones get flyFormation
-			formationLeader, formationSecond = script_formation.buildFormationIncremental(ship, index, formationLeader, formationSecond)
+	local station, total_used_strength = self:spawnShipAtPosition("Battlestation", pos)
+	station:orderStandGround()
+	for index = 1, 3 do
+		if total_used_strength >= strength then
+			break
 		end
-		-- maybe disable jump
-	else
-		script_hangar.create(ship[1], "Drone", 2)
+		local template = self:getClassTemplate("breakers")
+		local ship, used_strength = self:spawnShipAtPosition(template, pos)
+		ship:orderDefendTarget(station)
+		total_used_strength = total_used_strength + used_strength
+	end
+	total_used_strength = total_used_strength + self:addDrones(station, 2, 4, strength - total_used_strength)
+	return total_used_strength
+end
+
+
+--[[ Ghosts --]]
+
+EnemyModuleGhosts = EnemyModule:new{
+	faction="Ghosts",
+	classes={
+		fighters = arrayShuffle({
+			"Advanced Hornet",
+			"Advanced Lindworm",
+			"Advanced Adder MK5",
+			"Advanced Adder MK4",
+		}),
+		capitals = arrayShuffle({
+			"Phobos G4",
+			"Piranha G4",
+			"Nirvana 0x81",
+			"Solar Storm",
+		}),
+	}
+}
+
+function EnemyModuleGhosts:spawnEnemiesAtPositions(positions, strength)
+	--[[TODO better strategy
+		send a captial ship and two fighters
+	--]]
+	local position_index
+	while strength >= 0 do
+		position_index = position_index % #positions + 1
+		local formationLeader, formationSecond = nil, nil
+		local template = self:getClassTemplate("capitals")
+		local formationLeader, used_strength = self:spawnShipAtPosition(template, positions[position_index])
+		formationLeader:orderRoaming()
+		strength = strength - used_strength
+		for index = 2,3 do
+			if strength > 0 then
+				local template = self:getClassTemplate("fighters")
+				local ship, used_strength = self:spawnShipAtPosition(template, positions[position_index])
+				formationLeader, formationSecond = script_formation.buildFormationIncremental(ship, index, formationLeader, formationSecond)
+				strength = strength - used_strength
+			end
+		end
 	end
 end
+

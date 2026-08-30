aee0f42d9700afdc4eded9cfdfd2f4823c4d9a72
AyCe <anthony.cole.2@googlemail.com>
2026-08-30 05:12:51 +0200

    Simplified sliding tile puzzle minigame and made it prettier

--- src/screenComponents/newMinigames/slidingTilePuzzle.cpp
+++ src/screenComponents/newMinigames/slidingTilePuzzle.cpp
@@ -14,12 +14,70 @@ SlidingTilePuzzle::SlidingTilePuzzle(GuiPanel* owner, GuiHackingDialog* parent, 
 	// 3: 4x4
 	//width = 3 + difficulty / 2;
 	//height = 3 + (difficulty + 1) / 2;
-	width = 2 + (difficulty+1) / 2;
+	width = 2 + (difficulty + 1) / 2;
 	height = 3 + (difficulty) / 2;
+	if (irandom(0, 1) == 0)
+	{
+		std::swap(width, height);
+	}
+	map = std::vector<size_t>(width * height);
+	terrain = std::vector<SlidingTilePuzzle::TerrainInfo>();
+
+	// terrain:
+	terrain.push_back(SlidingTilePuzzle::TerrainInfo("corner", 0)/*"\xDA"*/); // top left
+	terrain.push_back(SlidingTilePuzzle::TerrainInfo("corner", 90)/*"\xBF"*/); // top right
+	terrain.push_back(SlidingTilePuzzle::TerrainInfo("", 0)); // center
+	terrain.push_back(SlidingTilePuzzle::TerrainInfo("edge", 0)/*"\xC4"*/); // top and bottom
+	terrain.push_back(SlidingTilePuzzle::TerrainInfo("edge", 90)/*"\xB3"*/); // left and right
+	terrain.push_back(SlidingTilePuzzle::TerrainInfo("corner", 270)/*"\xC0"*/); // bottom left
+	terrain.push_back(SlidingTilePuzzle::TerrainInfo("corner", 180)/*"\xD9"*/); // bottom right
+
 	for (int i = 0; i < width * height; i++)
 	{
-		int x, y;
-		std::tie(x, y) = tryGetCoords(i).value();
+		auto [x, y] = tryGetCoords(i).value();
+
+		// build a box
+		int terrain_id;
+		if (y == 0)
+		{
+			if (x == 0)
+			{
+				terrain_id = 0;
+			}
+			else if (x == width - 1)
+			{
+				terrain_id = 1;
+			}
+			else
+			{
+				terrain_id = 3;
+			}
+		}
+		else if (y == height - 1)
+		{
+			if (x == 0)
+			{
+				terrain_id = 5;
+			}
+			else if (x == width - 1)
+			{
+				terrain_id = 6;
+			}
+			else
+			{
+				terrain_id = 3;
+			}
+		}
+		else if (x == 0 || x == width - 1)
+		{
+			terrain_id = 4;
+		}
+		else
+		{
+			terrain_id = 2;
+		}
+		map[i] = terrain_id;
+
 		FieldItem* item = new FieldItem(owner, "", "", [this, i](bool b) { onFieldClick(i); });
 		item->setSize(50, 50);
 		item->setPosition(x * 50 - width * 25, 25 + y * 50 - height * 25, sp::Alignment::Center);
@@ -47,15 +105,17 @@ void SlidingTilePuzzle::reset()
 	std::vector<TileData> tiles {};
 	for (int i = 0; i < num_tiles; i++)
 	{
-		tiles.push_back(TileData(i, false));
+		tiles.push_back(TileData(map[i], false));
 	}
 
 	// pick the free spot (could be multiple in theory)
-	auto free_index = irandom(0, num_tiles - 1);
+	// auto free_index = irandom(0, num_tiles - 1);
+	// pick one of the inner tiles
+	auto free_index = tryGetIndex({ irandom(1, width - 2), irandom(1, height - 2) }).value();
 	tiles[free_index].is_free = true;
 
 	// shuffle
-	auto shuffles = 1000 * difficulty;
+	auto shuffles = 100 * difficulty;
 	while (shuffles-- > 0)
 	{
 		// move the free spot randomly, swap values
@@ -110,17 +170,27 @@ void SlidingTilePuzzle::checkGameState()
 	for (int i = 0; i < num_tiles; i++)
 	{
 		auto button = getFieldItem(i);
-		button->disable();
-		button->setValue(false);
-		auto tile = button->tile_data;
+		auto& tile = button->tile_data;
+
 		button->setVisible(!tile.is_free);
-		button->setText(tile.is_free ? "" : std::to_string(tile.index+1));
-		if (tile.is_free) {
+
+		if (tile.is_free)
+		{
 			num_free_tiles++;
 		}
-		else if (tile.index == i)
+		else
 		{
-			num_tiles_in_place++;
+			auto is_in_place = tile.terrain_index == map[i];
+
+			button->disable();
+			button->setValue(is_in_place);
+			auto& tile_terrain = terrain[tile.terrain_index];
+			button->setIcon(tile_terrain.sprite.empty() ? "" : "custom/newMinigames/slidingTilePuzzle/" + tile_terrain.sprite + ".png", sp::Alignment::Center, tile_terrain.rotation);
+
+			if (is_in_place)
+			{
+				num_tiles_in_place++;
+			}
 		}
 	}
 
@@ -131,6 +201,11 @@ void SlidingTilePuzzle::checkGameState()
 	if (num_tiles_in_place == num_tiles_that_count)
 	{
 		gameComplete();
+		for (int i = 0; i < num_tiles; i++)
+		{
+			auto button = getFieldItem(i);
+			button->setValue(false);
+		}
 	}
 	else
 	{
@@ -154,7 +229,6 @@ void SlidingTilePuzzle::checkGameState()
 					// this neighbour button is clickable! (will move into the free space)
 					auto neighbour = getFieldItem(neighbour_index.value());
 					neighbour->enable();
-					neighbour->setValue(true);
 				}
 			}
 		}
@@ -207,8 +281,7 @@ std::optional<std::pair<int, int>> SlidingTilePuzzle::tryGetCoords(int index) {
 
 std::optional<int> SlidingTilePuzzle::tryGetIndex(std::pair<int, int> coords)
 {
-	int x, y;
-	std::tie(x, y) = coords;
+	auto [x, y] = coords;
 	if (x < 0 || y < 0 || x >= width || y >= height)
 	{
 		return {};
@@ -218,9 +291,7 @@ std::optional<int> SlidingTilePuzzle::tryGetIndex(std::pair<int, int> coords)
 
 void SlidingTilePuzzle::moveCoords(std::pair<int, int>* coords, int direction)
 {
-	int x, y;
-	std::tie(x, y) = *coords;
-	//auto [x, y] = *coords;
+	auto& [x, y] = *coords;
 	switch (direction)
 	{
 	case 0: y--; break;
@@ -228,7 +299,6 @@ void SlidingTilePuzzle::moveCoords(std::pair<int, int>* coords, int direction)
 	case 2: y++; break;
 	case 3: x--; break;
 	}
-	*coords = { x,y };
 }
 
 SlidingTilePuzzle::FieldItem* SlidingTilePuzzle::getFieldItem(int idx)
@@ -237,11 +307,16 @@ SlidingTilePuzzle::FieldItem* SlidingTilePuzzle::getFieldItem(int idx)
 }
 
 SlidingTilePuzzle::FieldItem::FieldItem(GuiContainer* owner, string id, string text, func_t func)
-	: GuiToggleButton(owner, id, text, func), tile_data({ -1, false })
+	: GuiToggleButton(owner, id, text, func), tile_data({ 0, false })
 {
 }
 
-SlidingTilePuzzle::TileData::TileData(int index, bool is_free)
-	: index(index), is_free(is_free)
+SlidingTilePuzzle::TileData::TileData(size_t terrain_index, bool is_free)
+	: terrain_index(terrain_index), is_free(is_free)
+{
+}
+
+SlidingTilePuzzle::TerrainInfo::TerrainInfo(string sprite, float rotation)
+	: sprite(sprite), rotation(rotation)
 {
 }

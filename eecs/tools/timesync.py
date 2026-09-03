46d99cf77253e5a1857b8e350246ddc6193a82dc
Pithlit <pithlit@mail.de>
2026-09-03 10:45:23 +0200

    fleet utils

---
+++ eecs/tools/timesync.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import datetime
+import requests
+
+def exec(script, server="127.0.0.1:8080"):
+	return requests.post(f'http://{server}/exec.lua', script, timeout=5).content == b''
+
+def timesync(server):
+    now = datetime.datetime.now()
+    time = f"{now.hour}:{now.minute}"
+    code = f"""return getScriptStorage().vf_timesync:sync_time_human_readable("{time}")"""
+    exec(code, server=server+":8080")
+    print(f"sent time '{time}' to {server}")
+
+timesync("192.168.2.4")

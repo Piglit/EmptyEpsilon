f74d552344a4caa5036401423571b03ea1a1ba24
Pithlit <pithlit@mail.de>
2026-09-04 11:22:04 +0200

    bescheid-kampagne-fernschreiber works

--- eecs/outbound/pyroMessage.py
+++ eecs/outbound/pyroMessage.py
@@ -1,5 +1,11 @@
 from utils import pyrohelper
+from outbound.log import log
+import Pyro4
 
 def send(name, msg):
-	pyrohelper.connect_to_named(name).recv(msg)
+    try:
+        pyrohelper.connect_to_named(name).recv(msg)
+    except Pyro4.errors.NamingError as e:
+        log.error(f"pyro connect failed: {e}")
+        
 
